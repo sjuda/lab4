ee55b58f98e9c24c02b3127e1fe494a86e49fb29
sjuda <32189174+sjuda@users.noreply.github.com>
2017-10-13 11:31:48 +0300

    Create lab4.cpp

---
+++ lab4.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+    int main()
+    {
+        vector<double> vec;
+        double k = 0;
+        cout << "Enter first massive elements (when you done, enter '1000'): ";
+        do
+        {
+            cin >> k;
+            vec.push_back(k);
+        }
+        while(k < 1000);
+        vector<double> vect;
+        cout << "Enter double massive elements (when you done, enter '1000'): ";
+        double j = 0;
+        do
+        {
+            cin >> j;
+            vec.push_back(j);
+        }
+        while(j < 1000);
+
+        vector<double> vecto;
+        int b = 0;
+        do
+        {
+            vecto.push_back(vec[b]);
+            b++;
+        }
+        while(b < vec.size());
+        int c = 0;
+        do
+        {
+            vecto.push_back(vect[c]);
+            c++;
+        }
+        while(b < vect.size());
+        int z = 0;
+        for (int w =0;z+1 < vecto.size();w++ )
+        {
+            if (vecto[z] < vecto[z+1])
+            {
+                double vr = vecto[z];
+                vecto[z] = vecto[z+1];
+                vecto.push_back(vr);
+                z++;
+            }
+        }
+        int g = 0;
+        do
+        {
+            cout << g << " element is: " << vecto[g];
+            g++;
+        }
+        while (g < vecto.size());
+    return 0;
+}
