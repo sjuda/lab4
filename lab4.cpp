8ee7664ff8cfcd689ec90a5a031886ac67a04ce3
sjuda <32189174+sjuda@users.noreply.github.com>
2017-10-16 15:31:15 +0300

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -1,65 +1,61 @@
-#include <ostream>
 #include <vector>
 #include <stdio.h>
-#include <random>
-#include <cstdlib>
-#include <string>
 #include <iostream>
 
 using namespace std;
 int main()
 
 {
-    vector<double> vec;
-    double k = 0;
-    cout << "Enter first massive elements (when you donevalue biggest than 1000): "  << endl;
-    do
-    {
-        cin >> k;
-        vec.push_back(k);
+	vector<double> vec;
+	double k = 0;
+	cout << "Enter first massive elements (when you donevalue biggest than 1000): " << endl;
+	for (int i = 0; k < 1000; i+=1);
+	{
+		std::cin >> k;
+		vec.push_back(k);
 	}
-    while (k < 1000);
-    vector<double> vect;
-    cout << "Enter double massive elements (when you done, enter value biggest than 1000): " << endl;
-    double j = 0;
-    do
-    {
-        cin >> j;
-        vec.push_back(j);
-    }
-    while (j < 1000);
-    vector<double> vecto;
-    unsigned int b = 0;
-    do
-    {
-        vecto.push_back(vec[b]);
-        b++;
-    }
-    while (b < vec.size());
-    unsigned int c = 0;
-    do
-    {
-        vecto.push_back(vect[c]);
-        c++;
-    }
-    while (c < vect.size());
-    unsigned int z = 0;
-    for (int w = 0; z + 1 < vecto.size(); w++)
-    {
-        if (vecto[z] < vecto[z + 1])
-        {
-            double vr = vecto[z];
-            vecto[z] = vecto[z + 1];
-            vecto.push_back(vr);
-            z++;
-        }
-    }
-    unsigned int g = 0;
-    do
-    {
-        cout << g << " element is: " << vecto[g];
-        g++;
-    }
-    while (g < vecto.size());
-    return 0;
+
+	vector<double> vect;
+	cout << "Enter double massive elements (when you done, enter value biggest than 1000): " << endl;
+	double j = 0;
+	for (int i = 0; j < 1000; i++);
+	{
+		cin >> j;
+		vec.push_back(j);
+	}
+
+	vector<double> vecto;
+	unsigned int b = 0;
+	while (b < vec.size());
+	{
+		vecto.push_back(vec[b]);
+		b++;
+	}
+
+	unsigned int c = 0;
+	for (int i = 0; c < vect.size();i++);
+	{
+		vecto.push_back(vect[c]);
+		c++;
+	}
+
+	unsigned int z = 0;
+	for (int w = 0; z + 1 < vecto.size(); w++)
+	{
+		if (vecto[z] < vecto[z + 1])
+		{
+			double vr = vecto[z];
+			vecto[z] = vecto[z + 1];
+			vecto.push_back(vr);
+			z++;
+		}
+	}
+	unsigned int g = 0;
+	for (int i = 0; g < vecto.size(); i++);
+	{
+		cout << g << " element is: " << vecto[g];
+		g++;
+	}
+
+	return 0;
 }
